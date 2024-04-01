fe8fcc3afb991058c0236e2cf200a89120f87379
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-04-01 18:21:49 +0700

    Add project files.

---
+++ activitystruct/activitystruct.cpp
@@ -0,0 +1,13 @@
+// activitystruct.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
+};
+
